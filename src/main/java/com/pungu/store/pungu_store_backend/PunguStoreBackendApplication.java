52c6d1c6cfb0111f071e4f74ce69e93ebb213c01
Sachin Shakya <sshakya219@gmail.com>
2025-07-22 09:55:53 +0530

    Created a docker file for each service and completed the file storage implementation too. Somehow services are running on docker due to some issue, will look into that now.

---
+++ src/main/java/com/pungu/store/pungu_store_backend/PunguStoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pungu.store.pungu_store_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PunguStoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PunguStoreBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pungu/store/pungu_store_backend/PunguStoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pungu.store.pungu_store_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PunguStoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
